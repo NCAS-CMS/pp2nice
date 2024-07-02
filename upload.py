4ecde9a8f0abbba3f9e08933a75f9facf2f6d449
Bryan Lawrence <bryan.lawrence@ncas.ac.uk>
2024-07-02 06:48:19 +0100

    metadata check fixed

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from minio import Minio
-from s3v.s3core import get_user_config, sanitised_metadata
+from s3v.s3core import get_user_config, sanitise_metadata
 import os
 import tempfile
 import numpy as np
@@ -105,10 +105,11 @@ def do_verify(file_size, etag, client, bucket, object_name, metadata):
     # https://stackoverflow.com/questions/62555047/how-is-the-minio-etag-generated
     if metadata is not None:
         ometa = {k[11:]:v for k,v in result.metadata.items() if k.startswith('x-amz-meta')}
-        ometa = sanitised_metadata(sorted(ometa))
-        umeta = sanitised_metadata(sorted(metadata))
-        if ometa != umeta:
-            raise RuntimeError('Metadata not preserved - u{umeta} - o {ometa}')
+        ometa = sanitise_metadata(ometa)
+        umeta = sanitise_metadata(metadata)
+        for k in ometa:
+            if ometa[k]!=umeta[k]: 
+                raise RuntimeError('Metadata not preserved - u{umeta} - o {ometa}')
 
 def test_move_fail(target="hpos", bucket="bnl", secure=False):
     """
